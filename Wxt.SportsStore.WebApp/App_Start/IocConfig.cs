4e72dbf99d41e0794fab8286ef6441fea8269a39
xintian.wang23@gmail.com <xintian.wang23@gmail.com>
2019-03-30 17:48:13 -0400

    1 [CHG] add mock IProductsRepository

--- Wxt.SportsStore.WebApp/App_Start/IocConfig.cs
+++ Wxt.SportsStore.WebApp/App_Start/IocConfig.cs
@@ -1,10 +1,13 @@
 ﻿using Autofac;
 using Autofac.Integration.Mvc;
+using Moq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Wxt.SportsStore.Domain.Abstract;
+using Wxt.SportsStore.Domain.Entities;
 
 namespace Wxt.SportsStore.WebApp
 {
@@ -21,8 +24,19 @@ namespace Wxt.SportsStore.WebApp
             builder.RegisterControllers(AppDomain.CurrentDomain.GetAssemblies()).PropertiesAutowired();
             // Set the dependency resolver to be Autofac.
 
+            Mock<IProductsRepository> mock = new Mock<IProductsRepository>();
+            mock.Setup(m => m.Products).Returns(new List<Product>
+            {
+                new Product { Name = "Football", Price = 25 },
+                new Product { Name = "Surf board", Price = 179 },
+                new Product { Name = "Running shoes", Price = 95 }
+            });
+            builder.RegisterInstance<IProductsRepository>(mock.Object).PropertiesAutowired();
+
             var container = builder.Build();
             DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+
+
         }
     }
 }
