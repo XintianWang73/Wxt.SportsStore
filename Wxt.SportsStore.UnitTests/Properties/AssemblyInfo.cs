7c71e4547f55ce35d433c329c4252ccb2845333c
xintian.wang23@gmail.com <xintian.wang23@gmail.com>
2019-03-30 16:56:14 -0400

    construct top architecture
    1 [NEW] add webapp project
    2 [NEW] add domain project
    3 [NEW] add unittests project

---
+++ Wxt.SportsStore.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Wxt.SportsStore.UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("Wxt.SportsStore.UnitTest")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("37db577d-4a83-463e-8eee-ef70b844156e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
