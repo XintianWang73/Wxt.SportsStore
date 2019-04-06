08e772bcd086a5050b8197893318b4e6aac0355f
xintian.wang23@gmail.com <xintian.wang23@gmail.com>
2019-04-06 15:32:30 -0400

    Add debugconsole Add connect string

---
+++ Wxt.SportsStore.Domain/Concrete/EFDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wxt.SportsStore.Domain.Entities;
+
+namespace Wxt.SportsStore.Domain.Concrete
+{
+    class EFDbContext
+    {
+        public DbSet<Product> Products { get; set; }
+    }
+}
