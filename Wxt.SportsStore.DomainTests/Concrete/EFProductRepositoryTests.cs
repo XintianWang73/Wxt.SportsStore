b18988d479fd39c6242b9ada296026381e2c5097
xintian.wang1973@gmail.com <xintian.wang1973@gmail.com>
2019-05-11 23:08:40 -0400

    change connection string

---
+++ Wxt.SportsStore.DomainTests/Concrete/EFProductRepositoryTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Wxt.SportsStore.Domain.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wxt.SportsStore.Domain.Concrete.Tests
+{
+    [TestClass()]
+    public class EFProductRepositoryTests
+    {
+        [TestMethod()]
+        public void DeleteProductTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SaveProductTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
